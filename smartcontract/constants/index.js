59d2a3725e58759b7a88ba08feec43e3bc035136
Andrea C <mirthis@gmail.com>
2022-08-20 13:20:07 +0100

    first commit

---
+++ smartcontract/constants/index.js
@@ -0,0 +1,5 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS =
+  "0x289e89E9454f19C860f6cbc256445b99B9aEfDAf";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
